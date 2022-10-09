0f22993ccd061d02f9bf85a509ab1408e8352094
Chitosama <josecueto@Joses-Air.home>
2022-10-08 23:37:30 -0700

    Mission Start"

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Possible list of characters that can be used
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = uppercase.map(letter => letter.toLowerCase());
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
